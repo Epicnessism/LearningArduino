e174e1465424858b8dbd89fd46b596fe901d9669
epicnessism <twang9@gmu.edu>
2019-04-09 00:46:35 -0400

    inital test code created

--- controlPad/controlPad.ino
+++ controlPad/controlPad.ino
@@ -1,10 +1,34 @@
+//Tony Wang
+//GMU Mechanical Engineering Multidisciplinary: Senior Design 2018-2019
+//other pertinent info
+//sources and credits:....tbd
+
+#include <Keypad.h> //required for Keypad library to be used
+//reference link: https://playground.arduino.cc/Code/Keypad/
+//https://www.arduino.cc/en/Tutorial/RowColumnScanning
 
 
+const byte rows = 4; //four rows
+const byte cols = 3; //three columns
+char keys[rows][cols] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'#','0','*'}
+};
+
+byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[cols] = {8, 7, 6}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
+  char key = keypad.getKey();
 
+  if (key != NO_KEY){
+    Serial.println(key);
+  }
 }
